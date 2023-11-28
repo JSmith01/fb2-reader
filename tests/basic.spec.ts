73df4f7179b365e28524afefc522c7a0e321dbec
Dmitry Zlygin <dzlygin@gmail.com>
2023-11-28 16:55:56 -0500

    added e2e bookshelf test and basic navigation bar test

--- tests/basic.spec.ts
+++ tests/basic.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import path = require("path");
 
 test('has proper welcome page', async ({ page }) => {
@@ -11,7 +11,7 @@ test('has proper welcome page', async ({ page }) => {
   await expect(page.getByRole('button', { name: 'open bookshelf' })).toBeVisible();
 });
 
-test('loads the test fb2 book', async ({ page }, testInfo) => {
+test('loads the test fb2 book and navigate through it', async ({ page }, testInfo) => {
   await page.goto('http://localhost:3000/');
 
   await page.locator('#f').setInputFiles(path.join(__dirname, 'test.fb2.zip'));
@@ -21,6 +21,50 @@ test('loads the test fb2 book', async ({ page }, testInfo) => {
   const img = page.getByRole('img');
   await expect(img).toBeVisible();
 
-  const screenshot = await page.screenshot();
-  await testInfo.attach('screenshot', { body: screenshot, contentType: 'image/png' });
+  await testInfo.attach('screenshot', { body: await page.screenshot(), contentType: 'image/png' });
+
+  const [w, h] = await page.evaluate(() => [window.innerWidth, window.innerHeight]);
+  await page.mouse.click(w / 2, h - 10);
+
+  const curPercent = page.locator('#curPercent');
+  await expect(curPercent).toHaveText(/(49|50|51)\.\d+/);
+
+  await testInfo.attach('mid-book', { body: await page.screenshot(), contentType: 'image/png' });
+});
+
+test('test bookshelf', async ({ page }, testInfo) => {
+  await page.goto('http://localhost:3000/');
+
+  await page.locator('#f').setInputFiles(path.join(__dirname, 'test.fb2.zip'));
+
+  await expect(page.locator('#book > *')).toHaveCount(1);
+
+  page.on('dialog', dialog => dialog.accept());
+  await page.mouse.move(300, 5);
+  await page.locator('#close-book').click();
+
+  await expect(page.locator('#book > *')).toHaveCount(0);
+
+  const bookShelfMgr = page.locator('#bookshelf-manager');
+  await expect(bookShelfMgr).not.toBeVisible();
+  await page.getByRole('button', { name: 'open bookshelf' }).click();
+  await expect(bookShelfMgr).toBeVisible();
+
+  const shelfRows = page.locator('#bookshelf-manager table tr');
+  await expect(shelfRows).toHaveCount(1);
+  const removeBookFromShelfBtn = page.locator('#bookshelf-manager table tr button');
+  await expect(removeBookFromShelfBtn).toHaveCount(1);
+  await testInfo.attach('bookshelf-loaded', { body: await page.screenshot(), contentType: 'image/png' });
+
+  await removeBookFromShelfBtn.click();
+
+  await expect(shelfRows).toHaveCount(1);
+  await expect(removeBookFromShelfBtn).toHaveCount(0);
+  await testInfo.attach('bookshelf-empty', { body: await page.screenshot(), contentType: 'image/png' });
+
+  await page.getByRole('button', { name: 'Close bookshelf' }).click();
+
+  await expect(bookShelfMgr).not.toBeVisible();
+
+  await testInfo.attach('bookshelf-closed', { body: await page.screenshot(), contentType: 'image/png' });
 });
